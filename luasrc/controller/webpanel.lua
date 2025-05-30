01f476271d254879b74b88d7b0e24a34734c2e8a
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2025-05-30 16:14:30 +0800

    Update webpanel.lua

--- luasrc/controller/webpanel.lua
+++ luasrc/controller/webpanel.lua
@@ -1,7 +1,16 @@
 module("luci.controller.webpanel", package.seeall)
 
 function index()
-    entry({"admin", "services", "webpanel"}, firstchild(), _("Web Panels"), 60).dependent = false
-    entry({"admin", "services", "webpanel", "overview"}, template("webpanel/overview"), _("Overview"), 10)
-    entry({"admin", "services", "webpanel", "settings"}, cbi("webpanel/settings"), _("Settings"), 20)
+    entry({"admin", "services", "webpanel"}, firstchild(), _("Web Panels"), 60).index = true
+    entry({"admin", "services", "webpanel", "settings"}, cbi("webpanel/settings"), _("Settings"), 30)
+    
+    -- 动态注册面板页面
+    local uci = require "luci.model.uci".cursor()
+    uci:foreach("webpanel", "webpanel",
+        function(section)
+            entry({"admin", "services", "webpanel", section[".name"]}, 
+                  template("webpanel/panel/"..section[".name"]), 
+                  section.name or "Unnamed", 
+                  20)
+        end)
 end
