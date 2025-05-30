330046f9937b5fc6f9b597db513d4c9bc8210eff
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2025-05-30 15:06:49 +0800

    Create settings.lua

---
+++ model/cbi/webpanel/settings.lua
@@ -0,0 +1,14 @@
+local m = Map("webpanel", translate("Web Panel Settings"),
+    translate("Configure URLs of embedded web interfaces"))
+
+local s = m:section(TypedSection, "webpanel", "")
+s.addremove = true
+s.anonymous = false
+
+s:option(Value, "name", translate("Panel Name"))
+s:option(Value, "url", translate("URL"),
+    translate("Full URL including port (e.g. http://192.168.1.1:8080)"))
+s:option(Value, "width", translate("Width"), "100%")
+s:option(Value, "height", translate("Height"), "800px")
+
+return m
