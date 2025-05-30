a89d4502911485ee525a0296dfe44b8943b20cbb
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2025-05-30 17:08:41 +0800

    Update settings.lua

--- luasrc/model/cbi/webpanel/settings.lua
+++ luasrc/model/cbi/webpanel/settings.lua
@@ -1,17 +1,14 @@
-local m = Map("webpanel", translate("Web Panel Settings"),
-    translate("Configure URLs of embedded web interfaces"))
+local m = Map("webpanel", translate("Web Panel Settings"))
 
 local s = m:section(TypedSection, "webpanel", "")
 s.addremove = true
 s.anonymous = false
 
 s:option(Value, "name", translate("Panel Name"))
-s:option(Value, "url", translate("URL"),
-    translate("Full URL including port (e.g. http://192.168.1.1:8080)"))
-s:option(Value, "width", translate("Width"), "100%")
-s:option(Value, "height", translate("Height"), "800px")
-s:option(Value, "weight", translate("Sort Priority"), 
-    translate("Lower numbers appear first in menu"))
-.default = 100
+s:option(Value, "url", translate("URL"), 
+    "e.g. http://192.168.1.1:8080")
+s:option(Value, "width", translate("Width"), "100%").default = "100%"
+s:option(Value, "height", translate("Height"), "800px").default = "800px"
+s:option(Value, "weight", translate("Sort Priority")).default = 100
 
 return m
