0c42aa668ec635968bde3a3ceac5b6b9ae0360f2
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2025-08-06 01:29:44 +0800

    Update config.lua

--- files/usr/lib/lua/luci/model/cbi/webpanel/config.lua
+++ files/usr/lib/lua/luci/model/cbi/webpanel/config.lua
@@ -1,74 +1,58 @@
--- 
-m:append(Template("webpanel/cbi_overrides"))
+m = Map("webpanel", translate("Web Panels Manager"))
 
-m = Map("webpanel", translate("Web Panels Configuration"), 
-    translate("Configure multiple web panels to embed in LuCI interface"))
-
--- 全局设置
+-- 全局开关
 s = m:section(NamedSection, "global", "webpanel", translate("Global Settings"))
+s.anonymous = true
 s.addremove = false
 
-o = s:option(Flag, "enabled", translate("Enable Web Panels"))
-o.default = "1"
-o.rmempty = false
+o = s:option(Flag, "enabled", translate("Enable"))
+o.default = 1
 
--- 面板列表
-s = m:section(TypedSection, "panel", translate("Web Panels"), 
-    translate("List of configured web panels"))
+-- 面板列表（使用表格布局）
+s = m:section(TypedSection, "panel", translate("Panels"), 
+    translate("<strong>Tip:</strong> Add your web panels below"))
 s.template = "cbi/tblsection"
 s.addremove = true
 s.anonymous = false
-s.extedit = dispatcher.build_url("admin/services/webpanel/config/%s")
 
--- 表头定义 (确保列数匹配)
-s.sectionhead = translate("Panel Name")
-s.sectiondescription = translate("Panel Settings")
+-- 名称列（作为主键）
+name = s:option(Value, "name", translate("Name"))
+name.forcewrite = true
+name.rmempty = false
+name.width = "15%"
 
--- 名称 (作为section标题)
-function s.cfgsections(self)
-    local sections = {}
-    uci:foreach("webpanel", "panel", function(s) 
-        if s[".name"] then
-            sections[#sections + 1] = s[".name"]
-        end
-    end)
-    return sections
-end
+-- URL列
+url = s:option(Value, "url", translate("URL"))
+url.rmempty = false
+url.width = "25%"
+url.datatype = "string"
 
--- 各选项定义 (确保顺序和表头一致)
-o = s:option(Value, "name", translate("Name"))
-o.datatype = "string"
-o.rmempty = false
-o.width = "15%"
+-- 宽度/高度组
+wh = s:option(Value, "width", translate("Size"))
+wh.template = "cbi/webpanel_size"
+wh.width = "15%"
+wh:value("100%", "100%")
+wh:value("800px", "800px")
+wh:value("600px", "600px")
 
-o = s:option(Value, "url", translate("URL"))
-o.datatype = "string"
-o.rmempty = false
-o.width = "20%"
+-- 其他选项（使用紧凑布局）
+border = s:option(Flag, "border", translate("Border"))
+border.enabled = "1"
+border.disabled = "0"
+border.default = "1"
+border.width = "10%"
 
-o = s:option(Value, "width", translate("Width"))
-o.datatype = "string"
-o.default = "100%"
-o.width = "10%"
+scroll = s:option(Flag, "scrolling", translate("Scroll"))
+scroll.default = "1"
+scroll.width = "10%"
 
-o = s:option(Value, "height", translate("Height"))
-o.datatype = "string"
-o.default = "600px"
-o.width = "10%"
+refresh = s:option(Value, "refresh", translate("Refresh"))
+refresh.datatype = "uinteger"
+refresh.default = "0"
+refresh.width = "10%"
 
-o = s:option(Flag, "border", translate("Border"))
-o.default = "1"
-o.rmempty = false
-o.width = "10%"
-
-o = s:option(Flag, "scrolling", translate("Scrolling"))
-o.default = "1"
-o.rmempty = false
-o.width = "10%"
-
-o = s:option(Value, "refresh", translate("Refresh(s)"))
-o.datatype = "uinteger"
-o.default = "0"
-o.width = "10%"
+-- 隐藏的高度选项（由Size统一管理）
+hidden_height = s:option(HiddenValue, "height", "")
+hidden_height.default = "600px"
 
 return m
