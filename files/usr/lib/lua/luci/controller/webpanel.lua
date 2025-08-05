a427564a8b2830c901e300412f23a49906a71ac4
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2025-08-05 23:25:06 +0800

    Update webpanel.lua

--- files/usr/lib/lua/luci/controller/webpanel.lua
+++ files/usr/lib/lua/luci/controller/webpanel.lua
@@ -3,5 +3,15 @@ module("luci.controller.webpanel", package.seeall)
 function index()
     entry({"admin", "services", "webpanel"}, firstchild(), _("Web Panels"), 60).dependent = false
     entry({"admin", "services", "webpanel", "config"}, cbi("webpanel/config"), _("Configuration"), 1)
-    entry({"admin", "services", "webpanel", "view"}, template("webpanel/iframe"), _("View Panel"), 2)
+    entry({"admin", "services", "webpanel", "view"}, call("action_view"), _("View Panel"), 2)
+end
+
+function action_view()
+    local uci = luci.model.uci.cursor()
+    local url = uci:get("webpanel", "config", "url") or ""
+    
+    luci.template.render("webpanel/iframe", {
+        url = url,
+        edit_url = luci.dispatcher.build_url("admin/services/webpanel/config")
+    })
 end
